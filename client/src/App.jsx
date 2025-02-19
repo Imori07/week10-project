97d9875a8799ab02093cfda342dcea653a52ff12
Craig Clayton <craig__clayton@outlook.com>
2025-02-19 10:18:29 +0000

    add refetch for 7000 ms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,22 @@ import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
 function App() {
-  const { data: cryptos, isPending } = useQuery({
+  const {
+    data: cryptos,
+    isPending,
+    isFetching,
+  } = useQuery({
     queryKey: ['cryptos'],
     queryFn: fetchCryptos,
+    refetchInterval: 7000,
   });
+
   const coins = cryptos?.coins;
+
   return (
     <div>
       {isPending && <p>Loading cryptos...</p>}
+      {isFetching && <p>Fetching cryptos...</p>}
       <div>
         {coins?.map((coin) => (
           <div
@@ -19,7 +27,9 @@ function App() {
           >
             <p>{coin.item.name}</p>
             <img src={coin.item.thumb} alt={coin.item.name} />
-            <p>{coin.item.data.price}</p>
+            <p>
+              {coin.item.data.price} <span>{coin.item.symbol}</span>
+            </p>
             <p>{coin.item.data.price_btc} BTC</p>
           </div>
         ))}
