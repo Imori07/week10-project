76f4be1a138f4fee04abb0febc2d8562abb78ba1
Craig Clayton <craig__clayton@outlook.com>
2025-02-20 11:39:59 +0000

    delete server stuff, add a comparison without tanstack

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import CoinsWithTanstack from './components/CoinsWithTanstack.jsx';
+import CoinsWithout from './components/CoinsWithout.jsx';
+import './App.css';
+
+function App() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <div>
+      <button onClick={() => setShow((s) => !s)}>
+        {show ? 'hide' : 'show'}
+      </button>
+      {/* {show && <CoinsWithTanstack />} */}
+      {show && <CoinsWithout />}
+    </div>
+  );
+}
+
+export default App;
